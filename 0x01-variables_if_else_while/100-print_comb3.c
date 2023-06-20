1ba4a232dec77c8f657d571e1c9e0dd0e553e1e9
Milliam <milliamhonga@gmail.com>
2023-06-19 19:23:31 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of a single digit
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int r, n;
+
+	for (r = 48; r <= 56; r++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			putchar(r);
+			if (r != 56 || n != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
