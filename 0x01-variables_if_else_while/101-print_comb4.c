87760adc7a56d280cc28d4f68c0f25792489661f
Milliam <milliamhonga@gmail.com>
2023-06-19 19:48:56 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of a single digit
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int r, n, k;
+
+	for (r = 48; r < 58; r++)
+	{
+		for (n = 49; n < 58; n++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (k > n && n > r)
+				{
+					putchar(r);
+					putchar(n);
+					putchar(k);
+					if (r != 56 || n != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
