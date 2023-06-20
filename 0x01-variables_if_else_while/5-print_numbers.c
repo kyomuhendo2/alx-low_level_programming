2a1bda92fffbd2d3c6a3e32abe56788c11ae5fde
Milliam <milliamhonga@gmail.com>
2023-06-19 18:02:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints the digit numbers of base 10
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
