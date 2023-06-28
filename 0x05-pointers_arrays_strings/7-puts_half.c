3132377a5ea345fda9aafc06da670e72d333b508
Milliam <milliamhonga@gmail.com>
2023-06-27 18:59:38 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include"main.h"
+
+/**
+* puts_half -  print the last n characters of the string,
+* If n is odd
+* @str: string to print
+*
+* Return: null
+*/
+
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+	for (; k  < j; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
