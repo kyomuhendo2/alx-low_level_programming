6e8cac82e7b63d9df324ac6dc8b92bee12cde751
Milliam <milliamhonga@gmail.com>
2023-07-08 04:20:57 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,8 +12,6 @@ void print_times_table(int n)
 	int i, j, res;
 
 	if (n > 15 || n < 0)
-	{
-	}
 	else
 	{
 		for (i = 0; i <= n; i++)
@@ -21,34 +19,24 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				res = i * j;
-				if (res == 0)
+				if (j == 0)
 				{
-					if (i == 0 && j > 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
 					_putchar(res + '0');
-				}
-				else if (res <= 9 && j != 0)
+				} else if (res <= 9 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(res + '0');
-				}
-				else if (res >= 10 && res <= 99)
+				} else if (res >= 10 && res <= 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
-				}
-				else if (res > 99)
+				} else if (res > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
