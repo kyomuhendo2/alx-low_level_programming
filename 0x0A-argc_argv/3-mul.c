c18edf302f86207505bedbd85897e1b27cdaf641
Milliam <milliamhonga@gmail.com>
2023-07-12 02:56:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,14 @@ int main(int argc, char *argv[])
 {
 	int mul, x, y;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	mul = x * y;
-	if (argc == 3)
-	{
-		printf("%d\n", mul);
-	}
-	else
-	{
-		printf("Error\n");
-	}
+	printf("%d\n", mul);
 	return (0);
 }
