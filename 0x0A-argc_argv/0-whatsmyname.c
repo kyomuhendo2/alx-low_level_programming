04c5a801f60595af33822733c7b4f777ef6ff7fa
Milliam <milliamhonga@gmail.com>
2023-07-12 01:20:46 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,14 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void)argc;
 
-	for (i = 0; i < argc; i++)
-	{
-		if (i == 0)
-		{
-			printf("My name is: %s\n", argv[i]);
-		}
-	}
+	printf("My name is: %s\n", argv[0]);
 	return (0);
 }
