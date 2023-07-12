82dc5553f0c9ec114e257e3136fbf362888655c0
Milliam <milliamhonga@gmail.com>
2023-07-12 07:17:37 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,15 +22,18 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 	}
-	num_cents += cents / 25;
-	cents %= 25;
-	num_cents += cents / 10;
-	cents %= 10;
-	num_cents += cents / 5;
-	cents %= 5;
-	num_cents += cents / 2;
-	cents %= 2;
-	num_cents += cents;
-	printf("%d\n", num_cents);
+	else
+	{
+		num_cents += cents / 25;
+		cents %= 25;
+		num_cents += cents / 10;
+		cents %= 10;
+		num_cents += cents / 5;
+		cents %= 5;
+		num_cents += cents / 2;
+		cents %= 2;
+		num_cents += cents;
+		printf("%d\n", num_cents);
+	}
 	return (0);
 }
