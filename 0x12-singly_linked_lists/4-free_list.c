48dfff98693738ac709e3705bf787e2de9b10550
Milliam <milliamhonga@gmail.com>
2023-07-28 19:08:20 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ * @head: pointer to the head of a linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
